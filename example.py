ca9e93fcd057c6322d37f210ea4c20b718b54d30
cappe987 <andersson.casper99@gmail.com>
2019-08-28 17:26:57 +0200

    Addex example for display purposes.

---
+++ example.py
@@ -0,0 +1,151 @@
+
+if True:
+
+  if True:
+    retainMedium = 537293259
+
+    if True:
+      conditionWorking = 'Y'
+      upperNatural = 'F'
+
+    else:
+      uponIll = 'f'
+      talentDirty = False
+
+    passengerEmployee = 'L'
+    shoreHusband = False
+    lawsuitTheme = 236205479
+    distanceInitiative = -850766721
+
+  else:
+
+    if True:
+      boomStrange = False
+      somewhereTypical = "considerationProspect"
+
+    else:
+      welfareApproximately = -367616475
+      awayIndependent = True
+      investorFall = 148045045
+      reasonableCritical = "sueEmployer"
+
+    carAway = -425794112
+    paleClearly = True
+    nodAirline = "worksDraft"
+
+
+  if True:
+    physicalConfirm = False
+    reinforceRomantic = "usFuture"
+    severeBattle = -684735066
+    driverEnergy = False
+
+  else:
+    disappearHot = 277769686
+    gloveBench = 'T'
+    limitationSuppose = '8'
+
+  paintingDiscourse = 'G'
+  menuOperating = '~'
+  regulationKiss = "experienceInvestigator"
+
+else:
+
+  if False:
+
+    if False and False:
+      gasTopic = 919288940
+      substanceGo = 162191333
+      sincePretty = False
+
+    else:
+      foundGuess = 854755104
+      paymentBasketball = True
+
+    fenceNaturally = "graduateAmount"
+    utilityIncreasing = True
+
+  else:
+
+    if False or True:
+      blueReading = False
+      typicallyTry = -118633380
+      impressiveTragedy = 540690834
+
+    else:
+      shortDisorder = "viewBegin"
+      escapeMountain = '.'
+      nobodyFrequent = 's'
+
+    containerSink = "ntCrime"
+    referenceExplosion = True
+
+  embraceConvince = False
+  forestDiscrimination = "incredibleWarning"
+
+  if embraceConvince and embraceConvince:
+    baseFocus = 110019869
+    delayInside = "tissueFast"
+
+  else:
+    frenchBorn = 't'
+    approachChamber = "comprehensiveMedium"
+    engineerClean = 'X'
+
+  communicationPause = False
+  aidsMedia = 'L'
+  congressionalHuge = '/'
+  thirdAIDS = True
+  wrongSeat = False
+  singleCheap = True
+  interpretStorm = 'g'
+
+makerStake = "supplyMaker"
+
+if False:
+  mindDispute = '$'
+  tragedyDuring = 337913334
+
+  if True or True and False and True and True and False and False or False and True or False and True and True and False and False and True and False:
+    deriveOnce = "temperatureName"
+    acquireFeed = "executiveSky"
+
+  else:
+    tightLearn = 553555304
+    gradeInclude = 'm'
+
+  actionEngineer = "procedureEverybody"
+  accountDraw = '-'
+
+else:
+  scandalTaste = '@'
+  curiousDo = False
+
+  if curiousDo:
+    scoreAsset = -526121096
+    messageScience = 'r'
+    believeOven = "restAccept"
+
+  else:
+    beginningExpression = "settleAlmost"
+    pieceSet = 'l'
+
+  valueCEO = -589884122
+  relateRing = True
+  bulletTowards = False
+
+relationshipGuess = "wonderHomeless"
+
+if False:
+  airlineDirection = 644364510
+  processOfficer = -308837261
+  shopStrongly = ')'
+  criminalLibrary = '|'
+
+else:
+  taskShock = 219704405
+  woodDriver = False
+  fadeObjective = '5'
+
+possibleCash = -558271064
+chaseNone = False
