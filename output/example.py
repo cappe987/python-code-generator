7d797559021138a9c929a7c142260ce5b6803b1d
Casper Andersson <andersson.casper99@gmail.com>
2019-11-23 18:22:21 +0100

    Updated example with functions.

--- output/example.py
+++ output/example.py
@@ -1,309 +1,384 @@
+def generateInto(buildClub, failRoof, yardAccompany):
+  quietLetter = 13561
 
-if 1 > 6108  and  1 == 40114  or  True:
+  if 0 > 1  or  False:
+
+    if 1 > yardAccompany  and  1 < 81191  and  213305662 < 28276552  or  False:
+      failRoof = "implementAnniversary" + "cancerSpace" + "intentionExpose"
+      quietLetter = buildClub + 12592290 - 39873
+      buildClub = 0 % 1
+      buildClub = buildClub * buildClub
+
+    else:
+      islandPiece = 'H'
+      print(quietLetter)
+      islandPiece = 'o' + 'R'
+
+    quietLetter = 1326945 % quietLetter % 345
+
+  else:
+
+    if buildClub >= 89  and  True:
+      reviewCapture = 229471
+      failRoof = "settleEvent" + "falseFloor" + "suspectNervous"
+      print(reviewCapture)
+
+    else:
+      print(failRoof)
+      quietLetter = buildClub + yardAccompany - quietLetter
+
+    failRoof = failRoof + "suicideShooting" + "thoughtIsraeli"
+    failRoof = failRoof + "understandingError"
+    print(failRoof)
+
+  print(quietLetter)
+
+
+
+def defenseDown(easeBack, nobodyPush, recipeBedroom):
+
+  if 6148688 == 0  and  13350334 > easeBack  and  1 == 1  or  True:
+    print(nobodyPush)
+    print(nobodyPush)
+
+  easeBack = easeBack + 1070393999
+
+  if easeBack >= easeBack  or  easeBack <= 1  and  4914512 > easeBack  and  True:
+    expansionSoviet = True
+    easeBack = easeBack - 1
+
+  nobodyPush = easeBack <= easeBack  or  False
+
+
+
+def matterSettle(bombList, seeConcentration):
+
+  if 905132235 < seeConcentration  or  False:
+
+    if 3477630 >= 1  and  False:
+      bombList = '+' + 'O' + 'J'
+      print(bombList)
+      directionEar = "ohMusician"
+
+    else:
+      bombList = '}' + bombList + bombList
+      bombList = bombList + 'e' + '$'
+      bombList = ':' + 'Y' + bombList
+
+    if seeConcentration < seeConcentration  and  seeConcentration == 1  or  True:
+      supposedTrend = "systemProblem"
+      seeConcentration = 11005992 + seeConcentration
+      bombList = 'h' + 'A'
+
+    if 188070 == 0  and  133567249 >= seeConcentration  and  721532074 >= 106235  and  True:
+      bombList = 'x' + '7' + bombList
+      basisLanguage = "valueProminent"
+      bombList = 'p' + bombList
+      bombList = bombList + 'x'
+
+    else:
+      bombList = '0' + 'j'
+      bombList = '4' + bombList + bombList
+
+  print(bombList)
+  bombList = 't' + bombList
+  print(bombList)
+
+
+
+def eventuallyFreedom(roughlyPressure):
+  goalSmile = '<'
+
+  if 7121 >= 1  and  True:
+    print(goalSmile)
+    expensiveGreen = 1
+    expensiveGreen = 11 % 17436 % expensiveGreen
+
+  else:
+
+    if 3534107 >= 85415414  or  230 >= 1  or  True:
+      goalSmile = 'O' + goalSmile + 'I'
+      goalSmile = ']' + 'S'
+      destructionBase = "bellNegative"
+
+    if 1 >= 1  or  23419260 >= 26793560  or  False:
+      meaningPhase = 885344
+      goalSmile = goalSmile + 'X' + 'w'
+      print(meaningPhase)
+
+    else:
+      goalSmile = 'e' + 'L'
+      professorInfant = 1
+      professorInfant = 300353485 * professorInfant + professorInfant
+      print(goalSmile)
+
+  print(goalSmile)
+
+  if 205953156 >= 0  or  540203710 > 354842  or  1 == 203001656  and  True:
+
+    if 0 > 285070  and  True:
+      goalSmile = ' ' + goalSmile + '!'
+      goalSmile = 'b' + goalSmile
+      goalSmile = goalSmile + goalSmile + goalSmile
+      print(goalSmile)
+
+    else:
+      goalSmile = 'F' + 'y'
+      goalSmile = goalSmile + 's' + '['
+
+    if 1 == 12467964  or  False:
+      beautyChildhood = 33104
+      faceLove = "commanderPresentation"
+
+    else:
+      print(goalSmile)
+      print(goalSmile)
+      print(goalSmile)
+      print(goalSmile)
+
+    if 0 < 282  or  False:
+      stepKill = 2
+      goalSmile = '@' + 'P' + goalSmile
+      print(roughlyPressure)
+      roughlyPressure = "promoteConviction" + "desperateTown"
+
+    else:
+      mathReject = '4'
+      mathReject = 'v' + 'W'
+      goalSmile = goalSmile + mathReject + 'Y'
+
+  else:
+    print(goalSmile)
+
+    if 1 > 82  and  75444682 == 840  and  True:
+      print(goalSmile)
+      goalSmile = 'S' + goalSmile + '~'
+
+    if 17273 <= 1  or  False:
+      vsPotential = "occasionKey"
+      roughlyPressure = "classicDifficulty" + "expansionGroup" + "wordStart"
+
+
+
+def opponentDevice(cultureCompany):
+
+  if 1 > 1779162  and  True:
+
+    if 1058746091 <= 882347124  and  False:
+      print(cultureCompany)
+      cultureCompany = "facultyFactory" + cultureCompany
+
+    else:
+      cultureCompany = "grabMarket" + "consequencePractical"
+      likeAny = True
+
+    print(cultureCompany)
+
+  else:
+    budgetEstimate = 'H'
+
+    if 3290225 <= 1  and  True:
+      budgetEstimate = ' ' + budgetEstimate
+      budgetEstimate = '8' + 'a'
+      budgetEstimate = '<' + 'g'
+
+    else:
+      worriedConflict = '4'
+      competeHard = True
+      cultureCompany = "layerChanging" + "sexVillage"
+      shineNotion = "secureOften"
+
+  benefitDrink = True
+
+
+
+def tissueCloud(trustLaboratory, museumConflict):
+  museumConflict = '9' + '5' + museumConflict
+  compareImportant = False
+
+  if 0 > 0  or  44140079 <= 336766777  or  False:
+    print(museumConflict)
+    print(museumConflict)
+
+  else:
+
+    if 306249 <= 36642438  and  749 > 3452718  or  compareImportant:
+      museumConflict = '@' + '='
+      compareImportant = 0 < 772150629  and  False
+      museumConflict = 'b' + museumConflict + museumConflict
+
+    if 2222130 == 53991  and  False:
+      compareImportant = 79591015 <= 1  and  True
+      solidCultural = 0
+
+    print(museumConflict)
+
+
+
+def clinicalEditor(reporterGaze, comedyBean, babyDebt):
+
+  if 131030926 <= 919597  and  1076 >= 1  or  False:
+
+    if 386071 <= 86828930  and  0 >= 6  and  512 > 142274  and  comedyBean:
+      comedyBean = 0 > 227782385  and  comedyBean
+      retirementPattern = 1
+      reporterGaze = 'y' + 'U' + reporterGaze
+      reporterGaze = babyDebt + reporterGaze
+
+    else:
+      babyDebt = 'Q' + 'g' + '7'
+      comedyBean = 40001462 >= 6699616  and  False
+      angleEntry = True
+      accurateParticular = "weekendCoverage"
+
+    babyDebt = ' ' + '_' + 'Y'
+    comedyBean = 0 > 129  and  1 < 907593  or  True
+
+  comedyBean = 144 <= 701846611  or  3164 > 12  and  comedyBean
+
+  if 12 == 1940  or  0 > 6063  or  False:
+    print(babyDebt)
+
+    if 0 >= 385969513  and  0 >= 0  and  0 >= 1  or  False:
+      print(comedyBean)
+      degreeSolid = "eyePollution"
+
+    else:
+      comedyBean = 0 <= 0  or  289967 < 80  or  comedyBean
+      comedyBean = 2 > 146765  or  comedyBean
+
+    if 19431946 == 1386319  and  1 <= 534375  or  comedyBean:
+      babyDebt = reporterGaze + babyDebt
+      comedyBean = 2747310 <= 8  or  True
+
+    else:
+      babyDebt = 'J' + '>' + reporterGaze
+      comedyBean = 14556 < 1  and  False
+      comedyBean = 793 < 0  and  True
+
+    screamSong = 8800643
+
+  else:
+
+    if 1851 < 1  or  6764379 <= 0  and  True:
+      babyDebt = reporterGaze + 'm'
+      comedyBean = 1 >= 0  or  comedyBean
+
+    thinLiterary = "opportunityVersus"
+    reporterGaze = 'e' + '=' + '3'
+
+
+
+def locationExactly(criticalSave, techniqueInvasion, attributeScore):
+  print(attributeScore)
+
+  if 451722721 >= techniqueInvasion  and  criticalSave == criticalSave  or  331259 == 594960080  or  False:
+    criticalSave = criticalSave - 1 * criticalSave
+    kidProduce = 0
+    print(kidProduce)
+
+
+
+def poemPredict(additionalDrama):
+
+  if 1 >= 1  or  5866 == 69240844  or  False:
+
+    if 10714331 <= 0  and  0 < 2003  and  True:
+      additionalDrama = '*' + 'x' + additionalDrama
+      additionalDrama = additionalDrama + '2'
+      additionalDrama = '$' + additionalDrama
+      additionalDrama = '1' + additionalDrama
+
+    additionalDrama = 'J' + additionalDrama
+
+    if 151584483 >= 62418141  and  False:
+      additionalDrama = 'k' + 'E' + 'O'
+      poolUpper = "whereasResolution"
+
+  else:
+
+    if 39536738 > 0  or  False:
+      additionalDrama = 'B' + additionalDrama
+      additionalDrama = additionalDrama + additionalDrama + 'x'
+      additionalDrama = additionalDrama + additionalDrama
+      print(additionalDrama)
+
+    else:
+      additionalDrama = additionalDrama + additionalDrama
+      additionalDrama = additionalDrama + additionalDrama
+
+    if 0 <= 1  or  885634243 == 10075722  or  True:
+      print(additionalDrama)
+      additionalDrama = 'F' + '#' + 'F'
+      additionalDrama = 'v' + 'v'
+
+    if 415222 > 1  or  86093 >= 916138422  or  0 >= 160612225  or  False:
+      print(additionalDrama)
+      additionalDrama = ' ' + '^'
+
+    additionalDrama = 'W' + 'l'
+
+  if 0 > 263784  or  1 < 1  or  472366901 >= 525  and  True:
+    afternoonCareful = "mustInvasion"
+    print(additionalDrama)
+
+    if 273 >= 200  and  4604 < 0  and  False:
+      criticOkay = 460332
+      additionalDrama = 'e' + additionalDrama
+
+    else:
+      sequenceRelevant = 5847
+      afternoonCareful = "incredibleSeize" + "hunterFunding"
+      print(additionalDrama)
+
+    if 4707 >= 7369  and  2255849 >= 5  or  False:
+      additionalDrama = '0' + 'U' + additionalDrama
+      additionalDrama = additionalDrama + 'f' + 'N'
+      additionalDrama = '*' + 'G' + additionalDrama
+
+    else:
+      print(additionalDrama)
+      stickVia = 'O'
+
+  else:
+    additionalDrama = additionalDrama + additionalDrama + 'X'
+    print(additionalDrama)
+
+
+
+def tenGenetic(expectationShooting):
+
+  if 585 >= 70890  or  1 > 131884  or  False:
+    expectationShooting = 230037609 <= 0  or  103626 >= 0  or  expectationShooting
+    aheadThrow = 'K'
+
+  expectationShooting = 5170116 == 213340814  and  1 < 8759505  and  False
+
+
+
+print("--- Assignment Placeholder ---")
+
+if 1 < 522582647  or  634 <= 0  and  True:
+  incredibleMurder = "oughtMark"
+  entireDefine = 117
+
+  if 1346 <= 941854919  or  101504 < 29  or  entireDefine > entireDefine  or  True:
+    juniorTestify = 'I'
+    entireDefine = entireDefine + entireDefine - entireDefine
+
+  else:
+    itsOil = '4'
+    incredibleMurder = "dryPress" + incredibleMurder + "disorderModel"
+    print(incredibleMurder)
+    entireDefine = 42637 % 38150400 - entireDefine
+
+  entireDefine = 0 + entireDefine - 1
+
+else:
+
+  if 16322 <= 3834113  or  True:
+    expansionHell = 'b'
+    humanPsychologist = "plantFound"
+
   print("--- Assignment Placeholder ---")
-
-  if 184063 == 0  or  35735 == 637  or  True:
-    print("--- Assignment Placeholder ---")
-
-    if 36 > 373285987  or  False:
-      print("--- Assignment Placeholder ---")
-      funnyPrevious = "dramaticAssistance"
-      funnyPrevious = "halfJudgment" + "wingSlip"
-
-    else:
-
-      if 94885252 < 1  or  0 > 1  and  False:
-        print("supplyHimself")
-        print("--- Assignment Placeholder ---")
-        pregnancyCollective = False
-        pregnancyCollective = 974115 > 190577618  and  True
-
-      else:
-        print("creamKilling")
-        tenRemain = 1
-        immigrantRecruit = '3'
-
-      if 74593153 > 5193267  and  1 == 24381  or  True:
-        print("--- Assignment Placeholder ---")
-        print("platformObtain")
-        subsequentSky = 980
-
-      print("--- Assignment Placeholder ---")
-      print("stormYard")
-      print("--- Assignment Placeholder ---")
-
-    if 1 == 1  or  7 == 2  or  False:
-      print("neverthelessHead")
-
-      if 2 > 430748409  or  False:
-        print("--- Assignment Placeholder ---")
-        print("kickVessel")
-        print("--- Assignment Placeholder ---")
-        ballCharacteristic = False
-        ballCharacteristic = 154113 > 227766314  and  ballCharacteristic
-
-      catholicEase = 1
-
-    print("--- Assignment Placeholder ---")
-
-    if 0 <= 16198  or  False:
-      surprisedPrivacy = "supportBaseball"
-      attentionActor = "tissueSeven"
-
-      if 10 == 143207214  and  1 > 1  or  True:
-        attentionActor = "pantPrime" + attentionActor + "achievementEnormous"
-        attentionActor = surprisedPrivacy + surprisedPrivacy
-        doFewer = "yetKnee"
-
-      attentionActor = "alterRussian" + "wayPeace"
-
-  else:
-    print("--- Assignment Placeholder ---")
-
-    if 69537341 <= 86120765  and  True:
-
-      if 1013056013 < 1124  and  False:
-        print("--- Assignment Placeholder ---")
-        radicalPossess = True
-        thoseAnybody = '#'
-        radicalPossess = 1 <= 1  and  False
-        celebrityChannel = True
-
-      else:
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-        anniversaryPositive = '8'
-        print(anniversaryPositive)
-
-      gardenPeak = False
-      gardenPeak = 4697 <= 7384109  and  794217148 < 9  or  True
-
-      if 0 <= 7  and  False:
-        print(gardenPeak)
-        gardenPeak = 1 <= 13  and  False
-        gardenPeak = 615847 == 5084310  or  True
-
-      print(gardenPeak)
-
-    else:
-
-      if 480265745 == 1  and  197 == 8752031  and  True:
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-
-      else:
-        print("guessDirector")
-        print("yoursProceed")
-        print("--- Assignment Placeholder ---")
-
-      if 1 <= 6898639  and  3724644 > 1  or  True:
-        recommendProgram = False
-        recommendProgram = 101321491 > 26561  or  0 == 0  and  recommendProgram
-        recommendProgram = 1 == 98014  or  False
-        ahSpend = "operatorEncounter"
-
-      formalExecutive = 'b'
-      print(formalExecutive)
-
-    print("goldenPause")
-    print("employmentAM")
-    friendshipProceed = True
-
-  print("representativeDad")
-
-if 0 > 1  or  False:
-  print("amArgue")
-  print("norFee")
-  print("preciselyLegitimate")
-
-  if 1 <= 1  or  False:
-
-    if 207143 == 11973131  or  True:
-
-      if 28721096 < 336063186  or  True:
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-        collegeDecision = True
-
-      else:
-        print("--- Assignment Placeholder ---")
-        momentHelicopter = 101
-        momentHelicopter = 1242231 + 1
-        momentHelicopter = 9029728 + momentHelicopter / momentHelicopter
-
-      print("nationalMotor")
-      print("--- Assignment Placeholder ---")
-
-    else:
-
-      if 1 <= 715064  and  True:
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-
-      else:
-        print("--- Assignment Placeholder ---")
-        print("warningTelescope")
-        fullyComponent = 32450488
-        fullyComponent = fullyComponent / 4672627 + 2026
-
-      jumpQuarter = False
-      countySeveral = "coverageNatural"
-
-    if 3 <= 6216883  and  1 > 41354  or  False:
-      spiritLarge = '<'
-      print(spiritLarge)
-      ofStream = 1
-      ofStream = ofStream + ofStream
-
-    else:
-
-      if 35839 > 904  and  False:
-        print("--- Assignment Placeholder ---")
-        assessmentEstablish = "competePregnant"
-        investorDisappear = 'C'
-        carCivil = 767
-        assessmentEstablish = "spendWindow" + "strikeRecognize" + "promptCareful"
-
-      else:
-        print("--- Assignment Placeholder ---")
-        imaginationFinal = "beatDress"
-        print(imaginationFinal)
-        imaginationFinal = imaginationFinal + imaginationFinal
-
-      if 604136032 == 550638831  and  1605 > 2  and  False:
-        print("--- Assignment Placeholder ---")
-        print("sheetImplement")
-        print("--- Assignment Placeholder ---")
-        print("dustTake")
-        print("--- Assignment Placeholder ---")
-
-      else:
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-
-      busyHousehold = 467653
-
-      if 1498 < 0  or  True:
-        busyHousehold = 196171 % busyHousehold
-        busyHousehold = busyHousehold % 1 - 0
-        busyHousehold = 1456539 % busyHousehold
-        busyHousehold = busyHousehold * 281786408 - 58893
-        print(busyHousehold)
-
-      else:
-        busyHousehold = busyHousehold + 335597 * 0
-        print(busyHousehold)
-        hearEventually = 5297141
-        busyHousehold = 1 - 9
-
-    print("--- Assignment Placeholder ---")
-
-    if 17290 == 1  and  5772412 < 90588  or  True:
-      sinceIndustrial = False
-
-      if 1 < 22691687  and  sinceIndustrial:
-        print(sinceIndustrial)
-        sinceIndustrial = 52264456 == 1790  or  8 < 55  and  False
-        sinceIndustrial = 32108 == 1  and  False
-        sinceIndustrial = 280120 == 1294  and  True
-        print(sinceIndustrial)
-
-      if 501449 == 5457  or  True:
-        sinceIndustrial = 4959 == 0  or  sinceIndustrial
-        sinceIndustrial = 5 == 110999652  or  1362 < 0  and  True
-        versusCorn = "miracleUniverse"
-        sinceIndustrial = 3670 == 231  or  1 > 0  or  sinceIndustrial
-
-      else:
-        sinceIndustrial = 1 <= 586  and  29 > 0  or  False
-        sinceIndustrial = 0 > 1030849027  and  1 == 9  or  sinceIndustrial
-        sinceIndustrial = 1 < 301490  and  1 > 95044804  or  True
-        composeReplace = True
-
-  else:
-
-    if 90114198 > 1  or  True:
-
-      if 95652130 <= 260244  and  0 > 2587  and  True:
-        print("--- Assignment Placeholder ---")
-        print("oughtReply")
-        print("norCaptain")
-        print("viaAccess")
-
-      if 45836347 > 0  or  False:
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-        print("sayCard")
-        prospectBrain = 'o'
-
-      else:
-        voteReader = "denyFall"
-        voteReader = "mindChart" + voteReader + "shipGhost"
-        voteReader = voteReader + "correspondentUnder"
-
-      if 0 <= 13880479  or  True:
-        scheduleTour = False
-        scheduleTour = 238174 <= 6  or  220 > 320284120  and  False
-        scheduleTour = 0 < 1  and  scheduleTour
-        scheduleTour = 2 > 3918930  or  20664689 > 457  or  True
-
-      if 255 == 123426  or  False:
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-        print("--- Assignment Placeholder ---")
-        literaryFrench = True
-        literaryFrench = 0 <= 0  and  37456682 == 0  or  True
-
-    agoTerror = 'G'
-    agoTerror = agoTerror + '`' + '_'
-    print(agoTerror)
-
-if 1 == 1  or  False:
-
-  if 1 == 11  or  336678 <= 1  and  True:
-    print("--- Assignment Placeholder ---")
-
-    if 644534691 > 60  and  False:
-      patchOurselves = 891
-      print(patchOurselves)
-      patchOurselves = patchOurselves / patchOurselves % 7
-      print(patchOurselves)
-
-      if 20460722 <= 0  and  50967568 <= 33410  and  True:
-        print(patchOurselves)
-        patchOurselves = patchOurselves - patchOurselves % 1072912731
-        print(patchOurselves)
-
-      else:
-        patchOurselves = patchOurselves + 231274
-        shopOperator = True
-        patchOurselves = 4 - patchOurselves + 1
-        patchOurselves = 519078 - 18967 / 406763520
-
-    else:
-      chipGraduate = '`'
-
-      if 264400 < 792118078  and  444 > 1105  or  True:
-        chipGraduate = chipGraduate + chipGraduate
-        chipGraduate = 'T' + '9'
-        chipGraduate = 'R' + 'q' + 'b'
-
-      else:
-        chipGraduate = chipGraduate + 's' + '2'
-        print(chipGraduate)
-        chipGraduate = chipGraduate + 'E' + 'q'
-        locateColleague = 1
-
-      thosePublisher = True
-
-    waterLay = False
-
-  print("mainlySample")
-  completeBeginning = 'u'
 
